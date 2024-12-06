e585d1b8b1a17f16c4bc30db7c630d509599b090
Sdg877 <sdrakegill@gmail.com>
2024-12-06 10:45:52 +0000

    Contact form implemented

---
+++ frontend/src/ContactForm.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+
+const ContactForm = ({ existingContact = {}, updateCallback}) => {
+    const [firstName, setFirstName] = useState(existingContact.firstName || "");
+    const [lastName, setLastName] = useState(existingContact.lastName || "");
+    const [email, setEmail] = useState(existingContact.email || "");
+
+    const updating = Object.entries(existingContact).length !== 0
+
+    const onSubmit = async (e) => {
+        e.preventDefault()
+
+        const data = {
+            firstName,
+            lastName,
+            email
+        }
+
+        const url = "http://127.0.0.1:5000/" + (updating ? `update_contact/${existingContact.id}` : "create_contact")
+        const options = {
+            method: updating ? "PATCH" : "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        }
+        const response = await fetch(url, options)
+        if (response.status !== 201 && response.status !== 200) {
+            const data = await response.json()
+            alert(data.message)
+        } else {
+            updateCallback()
+        }
+    }
+
+    return (
+        <form onSubmit={onSubmit}>
+            <input
+                type="text"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                placeholder="First Name"
+            />
+            <input
+                type="text"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                placeholder="Last Name"
+            />
+            <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email"
+            />
+            <button type="submit">{updating ? "Update" : "Create"}</button>
+        </form>
+    );
+};
+
+export default ContactForm;
